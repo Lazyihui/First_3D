fc084044d0199bd7390ba0ff0e6554ba5b5a0686
包子 <chengbaozi6@qq.com>
2024-03-21 20:00:24 +0800

    <feature> ESTABLSH

---
+++ Assets/Scripts_Runtime/ClientMain.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Zelda
+{
+
+    public class ClientMain : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            Debug.Log("ClientMain Start");
+            Debug.Log("ClientMain Start 2");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
